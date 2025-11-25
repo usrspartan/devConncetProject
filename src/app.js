b428e9801b17a1eab9fbe211bf548c05b29e41e3
usrspartan <mailing2anup@gmail.com>
2025-11-25 14:59:06 +0530

    Added routes for testing

--- src/app.js
+++ src/app.js
@@ -2,18 +2,39 @@ const express = require('express');
 
 const app = express(); //creating an instance of a expressjs application
 
-app.use("/hello", (req,res)=>{
-    res.send("Hello Hello Hello Hello");  //Request handler function
+// app.use("/test", (req,res)=>{
+//     res.send("Hello from the server");  //Request handler function
+// })
+
+// app.get("/user",(req,res)=>{
+//     res.send({firstName:"Anupam", lastName:"Mishra"});
+// })
+
+app.post("/user", (req,res)=>{
+    console.log("Data posted successfully");
+    res.send("User details updated");
 })
 
-
-app.use("/nodemon", (req,res)=>{
-    res.send("Hello Hello Hello from  doremon");  //Request handler function
+app.delete("/user", (req,res)=>{
+    console.log("Deletion activated");
+    res.send("Item deleted successfully");
 })
 
+app.put("/user",(req,res) => {
+    res.send("User details updated using put");
+})
 
-app.use("/test", (req,res)=>{
-    res.send("Hello from the server");  //Request handler function
+app.patch("/user",(req,res)=>{
+    res.send("User details updated using patch");
+})
+
+app.get("/user",(req,res)=>{
+    console.log("Query part:: ", req.query);
+    
+})
+
+app.get("/user/:userId",(req,res)=>{
+    console.log("Params part:: ", req.params);   
 })
 
 app.listen(3000, ()=>{
